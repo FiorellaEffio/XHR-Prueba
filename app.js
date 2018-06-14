1f740cd4848c2f0b5114ea487353eb6e77faa8cf
FiorellaEffio <fiorella.effio@unmsm.edu.pe>
2018-06-13 19:12:20 -0500

    primera prueba, llamado archivo json nytimes

---
+++ app.js
@@ -0,0 +1,24 @@
+const form = document.getElementById('search-form');
+const searchField = document.getElementById('search-keyword');
+const responseContainer = document.getElementById('response-container');
+let searchedForText;
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  responseContainer.innerHTML = '';
+  searchedForText = searchField.value;
+  getNews();
+})
+function getNews() {
+  const articleRequest = new XMLHttpRequest();
+  articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=d50c09ba21dc4c1c967ab380d42c74cf`);
+  articleRequest.onload = addNews;
+  articleRequest.onerror = handleError;
+  articleRequest.send();
+}
+function handleError() {
+  console.log('Se ha presentado un error');
+}
+function addNews() {
+  const data = JSON.parse(this.responseText);
+  console.log(data);
+}
